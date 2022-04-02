1003eae38165b957a434a963e457b542ecba76e9
Chris Risner <crisner.prsvr@gmail.com>
2022-04-02 01:22:39 -0500

    added config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  }
 }
